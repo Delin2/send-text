75c297c253350bb06e580732e7809f0d177f1512
Dennis Lin <35812863+Delin2@users.noreply.github.com>
2019-04-14 20:41:44 -0400

    Update send_text.py

--- send_text.py
+++ send_text.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC196257a4ec2327393d7d3173bd711d57"
+account_sid = "????????"
 # Your Auth Token from twilio.com/console
-auth_token  = "613e599fdcf82d3661adbee14f73cb5e"
+auth_token  = "????????????"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+16467611839", 
-    from_="+19177460026",
-    body="Shhhhhhhhhh")
+    to="+01234567890", #who you wanna send to
+    from_="+01234567890", #twilio number
+    body="Send a Message")  #what you want to send
 
 print(message.sid)
